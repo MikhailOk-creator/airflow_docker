922d889af93c6c1b25623daf727990632173e07e
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2024-01-22 12:26:17 +0300

    Modified dag with PythonOperators
    3 functions have been added and work has been organized through xcom

--- dags/dag_with_python_operator.py
+++ dags/dag_with_python_operator.py
@@ -8,13 +8,37 @@ args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet():
-    print("Hello World!")
 
-with DAG(dag_id='dag_with_python', description='The first dag using python operator', schedule_interval='@daily',  start_date=datetime(2024, 10, 1), default_args=args) as dag:
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Roger')
+    ti.xcom_push(key='last_name', value='Waters')
+
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=80)
+
+
+def greet(ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    print(f"Hello World! My name is {first_name} {last_name}, and I am {age} years old")
+
+
+with DAG(dag_id='dag_with_python_V6', description='The first dag using python operator', schedule_interval='@daily',  start_date=datetime(2018, 11, 1), default_args=args) as dag:
     task1 = PythonOperator(
         task_id='greet',
         python_callable=greet
     )
 
-    task1
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name
+    )
+
+    task3 = PythonOperator(
+        task_id = 'get_age',
+        python_callable=get_age,
+    )
+
+    [task2, task3] >> task1
